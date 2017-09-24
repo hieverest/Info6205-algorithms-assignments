fdd2fae4408aedc182443cdc2c95ff1e74bbd49b
He_qianchen <hieverest@foxmail.com>
2017-09-24 15:54:00 -0700

    assignment 3

---
+++ assignment 3/PairSumClosestToZero.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class PairSumClosestToZero {
+    static public void pairSumClosestToZero(int[] arr) {
+        Arrays.sort(arr);
+
+        int start = 0, end = arr.length - 1;
+        int sum = Integer.MAX_VALUE;
+        int finalStart = -1;
+        int finalEnd = -1;
+
+        while(start < end) {
+            if(sum > Math.abs(arr[start] + arr[end])) {
+                sum = arr[start] + arr[end];
+                finalStart = start;
+                finalEnd = end;
+            }
+            if(arr[start] + arr[end] == 0) {
+                System.out.println("Sum = 0. Start = " + start + ", End = " + end);
+                return;
+            } else if(arr[start] + arr[end] < 0) {
+                start ++;
+            } else {
+                end --;
+            }
+        }
+        
+        System.out.println("Sum = " + sum + ", Start = " + finalStart + ", End = " + finalEnd);
+        
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -5, -3, -2, 1, 2, 3, 77, 88, 100 };
+        pairSumClosestToZero(arr);
+    }
+}
